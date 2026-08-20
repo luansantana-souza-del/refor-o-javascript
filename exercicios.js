dd998c30a6331267429aecbe80beb01c45b953db
LUAN SANTANA DE SOUZA <luan_santana-souza@estudante.sesisenai.org.br>
2026-08-20 20:44:28 -0300

    exercicio 10

--- exercicios.js
+++ exercicios.js
@@ -202,7 +202,10 @@ for (let i = 1; i <= 10; i++) {
 // Mostre cada número no console.
 // No final, mostre "Feliz Ano Novo!" (ou qualquer mensagem).
 // ------------------------------------------------------------
-
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+console.log("Feliz Ano Novo!");
 
 
 
