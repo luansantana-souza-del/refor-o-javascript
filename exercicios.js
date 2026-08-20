f14f534bbb70ea786fa11afddfaa4394fadc4f17
LUAN SANTANA DE SOUZA <luan_santana-souza@estudante.sesisenai.org.br>
2026-08-20 20:26:59 -0300

    exercicio5

--- exercicios.js
+++ exercicios.js
@@ -308,14 +308,6 @@ if (media >= 7) {
 // ------------------------------------------------------------
 
 
-
-
-// EXERCÍCIO 24 - Lista de tarefas
-// Esse exercício é a atividade principal!
-// Abra o arquivo script.js da atividade e complete os níveis.
-// ------------------------------------------------------------
-
-
 // ============================================================
 //  BLOCO 7 - DOM + LÓGICA COMBINADA (Desafio!)
 // ============================================================
