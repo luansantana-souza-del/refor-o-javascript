07b55387ddd4e5df2621bdc748dfb58f7067680d
LUAN SANTANA DE SOUZA <luan_santana-souza@estudante.sesisenai.org.br>
2026-08-20 21:17:32 -0300

    exercicio 23

--- exercicios.js
+++ exercicios.js
@@ -491,6 +491,30 @@ document.body.appendChild(botaoMais);
 // display: none
 // ------------------------------------------------------------
 
+const contadorHtml = document.createElement('h1');
+contadorHtml.textContent = '0';
+
+const botaoMenos = document.createElement('button');
+botaoMenos.textContent = '-';
+
+const botaoMais = document.createElement('button');
+botaoMais.textContent = '+';
+
+let valorContador = 0;
+
+botaoMais.onclick = function() {
+    valorContador++;
+    contadorHtml.textContent = valorContador;
+};
+
+botaoMenos.onclick = function() {
+    valorContador--;
+    contadorHtml.textContent = valorContador;
+};
+
+document.body.appendChild(contadorHtml);
+document.body.appendChild(botaoMenos);
+document.body.appendChild(botaoMais);
 
 // ============================================================
 //  BLOCO 7 - DOM + LÓGICA COMBINADA (Desafio!)
